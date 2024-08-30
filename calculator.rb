7890a8b4eed02f1af597bfe3364c42e569d0e8cb
Ajit J <ajit.scs@gmail.com>
2024-08-30 19:59:26 +0530

    Updated Calculator class as per the tests

--- calculator.rb
+++ calculator.rb
@@ -1,5 +1,9 @@
 class Calculator
   def add(numbers)
     raise ArgumentError, 'Input must be a string' unless numbers.is_a?(String)
+  
+    return 0 if numbers.blank?
+
+    numbers.split(',').map(&:to_i).sum
   end
 end
